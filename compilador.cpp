98de4f195acb16ac15fd851d87be55bda57274aa
Isaac-Avalos <naason.avalos@alumnos.udg.mx>
2023-03-11 13:04:23 -0600

    add and change variables

--- compilador.cpp
+++ compilador.cpp
@@ -2,6 +2,17 @@
 #include <string>
 #include <vector>
 
+
+const int ERROR = -1;
+const int ACPEPT = 99;
+int idx = 0;
+bool cERR = false;
+std::string token = "";
+std::string lexema = "";
+bool bPrinc = false;
+int line = 1;
+int col = 1;
+
 int main(){
     std::cout << "hola mundo!";
 
