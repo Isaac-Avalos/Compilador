62f6eff57b2242f2ef7c6893745737935ad0c288
Isaac-Avalos <naason.avalos@alumnos.udg.mx>
2023-03-11 13:37:56 -0600

    full transition table

--- compilador.cpp
+++ compilador.cpp
@@ -3,8 +3,8 @@
 #include <vector>
 
 
-const int ERROR = -1;
-const int ACPEPT = 99;
+const int ERR = -1;
+const int ACP = 99;
 int idx = 0;
 bool cERR = false;
 std::string token = "";
@@ -20,6 +20,21 @@ void compileError(std::string error_type, std::string desc){
 
 std::vector<std::vector<int>> transition_table = {
 
+    // let  dig del opa <   >   =   .   "
+    {   1,  2,  6,  5,  10, 8,  7, ERR, 12}, //0
+    {   1,  1, ACP, ACP,ACP,ACP,ACP,ACP,ACP}, // 1
+    {   ACP,2, ACP, ACP,ACP,ACP,ACP, 3, ACP}, // 2
+    {   ERR, 4,ERR,ERR,ERR,ERR,ERR,ERR, ERR}, // 3
+    {   ACP, 4, ACP,ACP,ACP,ACP,ACP,ACP, ACP}, // 4
+    {   ACP,ACP,ACP,ACP,ACP,ACP,ACP,ACP, ACP}, // 5
+    {   ACP,ACP,ACP,ACP,ACP,ACP,ACP,ACP, ACP}, // 6
+    {   ACP,ACP,ACP,ACP,ACP,ACP, 9, ACP, ACP}, // 7
+    {   ACP,ACP,ACP,ACP,ACP,ACP, 9, ACP, ACP}, // 8
+    {   ACP,ACP,ACP,ACP,ACP,ACP,ACP,ACP, ACP}, // 9
+    {   ACP,ACP,ACP,ACP,ACP, 11, 9, ACP, ACP}, // 10
+    {   ACP,ACP,ACP,ACP,ACP,ACP,ACP,ACP, ACP}, // 11
+    {   12,  12, 12, 12, 12, 12, 12, 12, 13}, // 12
+    {   ACP,ACP,ACP,ACP,ACP,ACP,ACP,ACP, ACP} // 13
 };
 
 int main(){
