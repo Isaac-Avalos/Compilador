1f6cfed2464e59674ce86cc6075d42896c410da3
Alan Castañeda <alanxyz.96@gmail.com>
2023-03-30 13:34:52 -0600

    Prototipos de funcion

--- compilador.cpp
+++ compilador.cpp
@@ -49,10 +49,67 @@ int matran[14][9] = {
     {ACP ,ACP ,ACP ,ACP ,ACP ,ACP ,ACP ,ACP ,ACP} //13
 };
 
-// Definicion de funciones
+// Prototipado de funciones
 
+void erra(string terr, string desc);
+int colCar(char x);
+pair<string, string> scanner();
+void cte();
+void termino();
+void signo();
+void expo();
+void multi();
+void suma();
+void oprel();
+void opno();
+void opy();
 void expr();
+void constVars();
+void params();
+void gpoExp();
+void leer(); // no
+void imprime(); // no
+void imprimenl(); // no
+void desde(); // no
+void mientras(); // no
+void si(); // no
+void repite(); // no
+void lmp(); // no
+void regresa(); // no
+void comando();
+void blkcmd();
 void estatutos();
+void blkFunc();
+void funcs();
+void prgm();
+void parser();
+
+// -------------------------------------------------------------
+// Funcion principal
+int main() {
+    string arche = "";
+    cout << "Archivo (.icc) [.] = salir: ";
+    cin >> arche;
+    if (arche == ".") exit(0);
+    ifstream archivo(arche);
+
+    // Carga de archivo en entrada
+    string entrada = "";
+    string linea = "";
+    while (getline(archivo, linea)) {
+        entrada += linea + "\n";
+    }
+
+    cout << entrada << endl;
+    parser();
+    if (!cERR) cout << "Programa COMPILO con EXITO" << endl;
+
+    return 0;
+}
+// Funcion principal
+// -------------------------------------------------------------
+
+// Definicion de las funciones
 
 void erra(string terr, string desc) {
     cERR;
@@ -395,28 +452,3 @@ void prgm() {
 void parser() {
     prgm();
 }
-
-
-// -------------------------------------------------------------
-// Funcion principal
-int main() {
-    string arche = "";
-    cout << "Archivo (.icc) [.] = salir: ";
-    cin >> arche;
-    if (arche == ".") exit(0);
-    ifstream archivo(arche);
-
-    // Carga de archivo en entrada
-    string entrada = "";
-    string linea = "";
-    while (getline(archivo, linea)) {
-        entrada += linea + "\n";
-    }
-
-    cout << entrada << endl;
-    parser();
-    if (!cERR) cout << "Programa COMPILO con EXITO" << endl;
-
-    return 0;
-}
-// -------------------------------------------------------------
